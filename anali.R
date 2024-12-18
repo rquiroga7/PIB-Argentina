d5a86bdeabac828b96e7d3fd469107b97680d0d0
rquiroga7 <rquiroga@unc.edu.ar>
2024-12-18 15:28:31 -0300

    traduzco comentarios

--- anali.R
+++ anali.R
@@ -1,35 +1,37 @@
-# Load necessary libraries
+# Cargar las bibliotecas necesarias
 library(tidyverse)
 library(scales)
 library(ggplot2)
-# Read the CSV file
+library(zoo)
+
+# Leer el archivo CSV
 data <- read_csv("sh_VBP_VAB_12_24.csv",col_names = FALSE, skip=2)
 
-# Identify columns to keep: column 1 and those not multiples of 6 or 6 + 1
+# Identificar las columnas a mantener: columna 1 y aquellas que no son múltiplos de 6 o 6 + 1
 columns_to_keep <- c(1, which(!(seq_along(data) %% 6 %in% c(0, 1))))
 
-# Subset the data to keep only the identified columns
+# Subconjunto de datos para mantener solo las columnas identificadas
 data <- data[, columns_to_keep]
 
-#Remove rows 1 and 3
+# Eliminar las filas 1 y 3
 data <- data[-c(1,3),]
-#Replace NA values with ""
+# Reemplazar valores NA con ""
 data[is.na(data)] <- ""
 
-#New dataframe combining data with years and quarters
-# Replace row 2 with year row, starting at 2004, repeating 4 times each, except for 2024
+# Nuevo dataframe combinando datos con años y trimestres
+# Reemplazar la fila 2 con la fila de años, comenzando en 2004, repitiendo 4 veces cada uno, excepto para 2024
 years <- c("",rep(2004:2023, each = 4),2024,2024,2024)
 quarters<-c("",rep(c("Q1","Q2","Q3","Q4"),times=20),c("Q1","Q2","Q3"))
 data2 <- data.frame(rbind(years,quarters,data))
 
-# Extract year and quarter information
+# Extraer información de años y trimestres
 years2 <- as.vector(data2[1, -1])
 quarters2 <- as.vector(data2[2, -1])
 
-# Extract name information
+# Extraer información de nombres
 names <- as.vector(data2[-c(1, 2), 1])
 
-# Pivot to long format
+# Pivotar a formato largo
 data_long <- data2[-c(1, 2), ] %>%
   pivot_longer(cols = -X1, names_to = "column", values_to = "value") %>%
   mutate(year = as.numeric(rep(years2, times = length(names))),
@@ -40,7 +42,7 @@ data_long <- data_long %>%
   mutate(year = as.numeric(year),
          value = as.numeric(value))
 
-#Create new name variable "Industria manufacturera sin alimentos", by substracting "Elaboración de productos alimenticios y bebidas" from "Industria manufacturera"
+# Crear nueva variable de nombre "Industria manufacturera sin alimentos", restando "Elaboración de productos alimenticios y bebidas" de "Industria manufacturera"
 data_long <- data_long %>%
   group_by(year, quarter) %>%
   mutate(value = if_else(name == "Industria manufacturera", 
@@ -49,14 +51,14 @@ data_long <- data_long %>%
          name = if_else(name == "Industria manufacturera", "Industria manufacturera sin alimentos", name)) %>%
   ungroup()
 
-#Shorten some names using case_when
+# Acortar algunos nombres usando case_when
 data_long <- data_long %>%
   mutate(name = case_when(
     name == "Fabricación de vehículos automotores, remolques y semirremolques" ~ "Fabricación de vehículos",
     name == "Extracción de minerales metalíferos. Explotación de minas y canteras n.c.p." ~ "Minerales metalíferos",
     TRUE ~ name))
 
-# Define the plotting function
+# Definir la función de trazado
 plot_evolution <- function(data, names, filename,quarters) {
   data %>%
     filter(name %in% names) %>%
@@ -77,8 +79,8 @@ plot_evolution <- function(data, names, filename,quarters) {
     ggsave(paste0(filename,"_",quarters,".png"), dpi = 300)
 }
 
-# Create the plots
-for (quarter in c("Q2", "Q3")) {
+# Crear los gráficos
+for (quarter in c("Q3")) {
   plot_evolution(data_long, c("Agricultura, ganadería, caza y silvicultura", "Elaboración de productos alimenticios y bebidas", "Industria manufacturera sin alimentos", "Comercio mayorista, minorista y reparaciones"), "grandes", quarter)
   
   # suben
@@ -108,7 +110,7 @@ plot_evolutionall <- function(data, names, filename) {
     ggtitle(paste0("VAB a Precios Básicos por sector económico para cada trimestre 2004-2024")) +
     facet_wrap(~name, scales = "free_y", ncol = 2) +
     theme_light() +
-    #Label each third quarter with a dot
+    # Etiquetar cada tercer trimestre con un punto
     geom_point(data = data %>%  filter(name %in% names) %>% filter(quarter == "Q3"), aes(x = fecha, y = value,color=name), size = 2) +
     scale_y_continuous(labels = comma) +
     zoo::scale_x_yearqtr(format = '%Y-T%q',expand = c(0, 0.22), minor_breaks = NULL, breaks = seq(2004.50, 2024.50, 1)) +
